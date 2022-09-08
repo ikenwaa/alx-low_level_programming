4785d952f8d5298c1567a4b22218d9c9c1923746
ikenwaa <ajohn9353@gmail.com>
2022-09-08 16:58:43 -0700

    Added 5-printf.c file

---
+++ 0x00-hello_world/5-printf.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+
+/**
+ *
+ * main - returns the string "with proper grammar, but the outcome is a piece of art," followed by a new line.
+ *
+ * Return - 0 if program is true, otherwise return something else
+ *
+ */
+
+int main(void)
+{
+	printf("with proper grammar, but the outcome is a piece of art,\n");
+	return (0);
+}
