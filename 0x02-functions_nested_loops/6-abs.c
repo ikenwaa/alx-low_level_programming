b167da45af1d1237d195f69fa76a2231f3311231
ikenwaa <ajohn9353@gmail.com>
2022-09-13 18:40:24 -0700

    Added 6-abs

--- 0x02-functions_nested_loops/6-abs.c
+++ 0x02-functions_nested_loops/6-abs.c
@@ -3,6 +3,8 @@
 /**
  * _abs - computes the absolute value of an integer.
  * @i: integer param
+ *
+ * Return: 0 always
  */
 
 int _abs(int i)
@@ -10,5 +12,5 @@ int _abs(int i)
 	if (i >= 0)
 		return (i);
 	else
-		return(-i);
+		return (-i);
 }
