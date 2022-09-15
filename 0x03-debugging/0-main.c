1d21f715c4ddc304325a46b631fef8800438534f
ikenwaa <ajohn9353@gmail.com>
2022-09-15 11:32:18 -0700

    Added 0-main.c

---
+++ 0x03-debugging/0-main.c
@@ -0,0 +1,16 @@
+#include "main.h"
+
+/**
+ * main - tests function that prints if integer is pos or neg
+ * Return: 0
+ */
+
+int main(void)
+{
+	int i;
+
+	i = 150;
+	positive_or_negative(i);
+
+	return (0);
+}
