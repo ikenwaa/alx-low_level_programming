2e92366e56af15e08cdb0cf1bbc7d07943862c5d
ikenwaa <ajohn9353@gmail.com>
2022-09-15 15:42:04 -0700

    Added 2-mul.c

---
+++ 0x04-more_functions_nested_loops/2-mul.c
@@ -0,0 +1,14 @@
+#include "main.h"
+
+/**
+ * mul - multiplies two integers
+ * @a: first integer
+ * @b: second integer
+ * Return: multiple of a and b
+ */
+
+int mul(int a, int b)
+{
+	if ((a >= 48 && a <= 57) && (b >= 48 && b <= 57))
+		return (a * b);
+}
