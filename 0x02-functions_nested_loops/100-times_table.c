5678e5d7ffb4c3514eace6e86af3e0128efd9b3e
ikenwaa <ajohn9353@gmail.com>
2022-09-14 18:27:47 -0700

    Added 100-print_times_table.c

--- 0x02-functions_nested_loops/100-times_table.c
+++ 0x02-functions_nested_loops/100-times_table.c
@@ -28,11 +28,13 @@ void print_times_table(int n)
 					_putchar(44);
 					_putchar(32);
 					_putchar(32);
+					_putchar(32);
 					_putchar((k / 10) + 48);
 					_putchar((k % 10) + 48);
 				} else if (k >= 100)
 				{
 					_putchar(44);
+					_putchar(32);
 					_putchar(32);
 					_putchar((k / 100) + 48);
 					_putchar(((k / 10) % 10) + 48);
