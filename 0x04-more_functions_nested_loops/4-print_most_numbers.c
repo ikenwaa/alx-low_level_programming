10269ecdf55792d5d9d62c064e291f21586a13ed
ikenwaa <ajohn9353@gmail.com>
2022-09-15 16:06:15 -0700

    Added 4-print_most_numbers.c

--- 0x04-more_functions_nested_loops/4-print_most_numbers.c
+++ 0x04-more_functions_nested_loops/4-print_most_numbers.c
@@ -6,11 +6,11 @@
 
 void print_most_numbers(void)
 {
-	int i = 0;
+	int i;
 
-	while (i < 10)
+	for (i = 0; i < 10; i++)
 	{
-		if (i == 2 || i == 4)
+		if (i == 2 && i == 4)
 			continue;
 		_putchar(48 + i);
 	}
