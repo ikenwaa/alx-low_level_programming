96a1e2a49fb82a2385d8aff956d10873be376d55
ikenwaa <ajohn9353@gmail.com>
2022-09-14 19:33:48 -0700

    Added 104-fibonacci.c

---
+++ 0x02-functions_nested_loops/104-fibonacci.c
@@ -0,0 +1,33 @@
+#include <stdio.h>
+
+/**
+ * main - finds and prints the first 98 fib numbers
+ * starting with 1 and 2
+ *
+ * Return: always 0
+ */
+
+int main(void)
+{
+	int count = 2;
+	long int i, j, k;
+
+	i = 1;
+	j = i + 1;
+	k = i + j;
+
+	printf("%lu, ", i);
+	printf("%lu, ", j);
+
+	while (count < 98)
+	{
+		count++;
+		printf("%d", k);
+		i = j;
+		j = k;
+		k = i + j;
+		if (count < 98)
+			printf(", ");
+	}
+	printf("\n");
+}
