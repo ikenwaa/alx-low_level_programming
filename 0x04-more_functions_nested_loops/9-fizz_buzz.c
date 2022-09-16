237fe2ec4c78340402f70bc899ad514075b6530c
ikenwaa <ajohn9353@gmail.com>
2022-09-15 18:41:03 -0700

    Added 9-fizz_buzz.c

--- 0x04-more_functions_nested_loops/9-fizz_buzz.c
+++ 0x04-more_functions_nested_loops/9-fizz_buzz.c
@@ -16,9 +16,9 @@ int main(void)
 {
 	int i = 1;
 
-	for (; i < 100; i++)
+	for (; i <= 100; i++)
 	{
-		if (i % 3 == 0 & i % 5 == 0)
+		if (i % 3 == 0 && i % 5 == 0)
 			printf("FizzBuzz ");
 		else if (i % 3 == 0)
 			printf("Fizz ");
@@ -27,7 +27,7 @@ int main(void)
 		else
 			printf("%d ", i);
 	}
-	printf("Buzz\n");
+	printf("\n");
 
 	return (0);
 }
