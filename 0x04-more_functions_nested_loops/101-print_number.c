f010400670dec29e4d23c86eb435ce55413f1756
ikenwaa <ajohn9353@gmail.com>
2022-09-16 18:21:08 -0700

    Added 101-print_number.c

---
+++ 0x04-more_functions_nested_loops/101-print_number.c
@@ -0,0 +1,25 @@
+#include "main.h"
+
+/**
+ * print_number - prints a number on the terminal
+ * @n: number to print
+ * Return: 0 always
+ */
+
+void print_number(int n)
+{
+	unsigned int num;
+
+	if (n <= 0)
+	{
+		num = -n;
+		_putchar(45);
+	}
+	else
+		num = n;
+
+	if (num / 10)
+		print_number(num / 10);
+	_putchar((num % 10) + 48);
+	return (0);
+}
