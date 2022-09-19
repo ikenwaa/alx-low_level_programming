0ee1f9325cdc4cc43e91ec377ed7f6fedb049051
ikenwaa <ajohn9353@gmail.com>
2022-09-19 15:55:17 -0700

    Added 2-strlen.c

---
+++ 0x05-pointers_arrays_strings/2-strlen.c
@@ -0,0 +1,17 @@
+#include "main.h"
+
+/**
+ * _strlen - This func returns the length of a string.
+ * @s: String for which length is to be returned
+ *
+ * Return: length of string.
+ */
+
+int _strlen(char *s)
+{
+	int len = 0;
+
+	while (*)s + len) != '\0')
+		len++;
+	return (len);
+}
