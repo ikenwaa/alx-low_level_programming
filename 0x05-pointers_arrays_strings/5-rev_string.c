5fbdc4eb803954a60f6aceae01e94b45c4248f92
ikenwaa <ajohn9353@gmail.com>
2022-09-19 16:42:49 -0700

    Added 5-rev_string.c

---
+++ 0x05-pointers_arrays_strings/5-rev_string.c
@@ -0,0 +1,17 @@
+#include "main.h"
+
+/**
+ * rev_string - Reverses a string and prints to stdout
+ * @s: String to be reversed
+ */
+
+void rev_string(char *s)
+{
+	int len - strlen(s);
+
+	for (i = len; i >= 0; i--)
+	{
+		_putchar(*s + i);
+	}
+	_putchar('\n');
+}
