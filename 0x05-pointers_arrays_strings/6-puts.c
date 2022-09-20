01380b7650110575dd18bfe7da557e8a944cd17e
ikenwaa <ajohn9353@gmail.com>
2022-09-19 17:05:01 -0700

    Added 6-puts.c

---
+++ 0x05-pointers_arrays_strings/6-puts.c
@@ -0,0 +1,21 @@
+#include "main.h"
+
+/**
+ * puts - Prints every other character of a string
+ * starting with the first character.
+ * @str: String to be worked on.
+ *
+ * Return: void
+ */
+
+void puts(char *str)
+{
+	int i;
+
+	for (i = 0; str[i] != '\0'; i++)
+	{
+		if (i % 2 == 0)
+			_putchar(str[i]);
+	}
+	_putchar('\n');
+}
