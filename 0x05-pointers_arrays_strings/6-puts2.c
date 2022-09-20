e9891bc25aa176a59f15dccfd8dedace01cd84c5
ikenwaa <ajohn9353@gmail.com>
2022-09-19 17:08:22 -0700

    Added 6-puts2.c

--- 0x05-pointers_arrays_strings/6-puts2.c
+++ 0x05-pointers_arrays_strings/6-puts2.c
@@ -1,7 +1,7 @@
 #include "main.h"
 
 /**
- * puts - Prints every other character of a string
+ * puts2 - Prints every other character of a string
  * starting with the first character.
  * @str: String to be worked on.
  *
