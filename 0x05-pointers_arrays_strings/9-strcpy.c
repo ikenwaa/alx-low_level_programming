a8cc3914e197c8793c9a509e7a1b849ca9548b20
ikenwaa <ajohn9353@gmail.com>
2022-09-19 18:07:41 -0700

    Added 9-strcpy.c

--- 0x05-pointers_arrays_strings/9-strcpy.c
+++ 0x05-pointers_arrays_strings/9-strcpy.c
@@ -11,9 +11,9 @@ char *_strcpy(char *dest, char *src)
 {
 	int i;
 
-	for (i = 0; *(src + 1) != '\0'; i++)
+	for (i = 0; *(src + i) != '\0'; i++)
 	{
-		dest[i] = *(src + 1);
+		dest[i] = *(src + i);
 	}
 	dest[i] = '\0';
 
