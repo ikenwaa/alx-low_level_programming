872953831775db8b8179994b0216f8e0a75b9640
ikenwaa <ajohn9353@gmail.com>
2022-09-19 18:58:14 -0700

    Added 101.keygen.c

---
+++ 0x05-pointers_arrays_strings/101-keygen.c
@@ -0,0 +1,26 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+
+/**
+ * main - Generates keygen
+ * Return: 0 always
+ */
+
+int main(void)
+{
+	int r = 0, i = 0;
+	time_t t;
+
+	srand((unsigned int) time_t(&t));
+	while (i < 2772)
+	{
+		r = rand() % 128;
+		if((i + r) > 2772)
+			break;
+		i += r;
+		printf("%c", r);
+	}
+	printf("%c\n", (2772 - i));
+	return (0);
+}
