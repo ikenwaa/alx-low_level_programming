4f83daf02498ce62dfb8c3a7b88d8eee813fa32c
ikenwaa <ajohn9353@gmail.com>
2022-09-22 15:43:44 -0700

    Added 6-cap_string.c

--- 0x06-pointers_arrays_strings/6-cap_string.c
+++ 0x06-pointers_arrays_strings/6-cap_string.c
@@ -11,17 +11,17 @@ char *cap_string(char *str)
 	int cnt = 0, i;
 	int word_sep[] = {32, 9, 10, 44, 59, 46, 33, 63, 34, 40, 41, 123, 125};
 
-	if (*(s + cnt) > 96 && *(s + cnt) < 123)
-		*(s + cnt) -= 32;
+	if (*(str + cnt) > 96 && *(str + cnt) < 123)
+		*(str + cnt) -= 32;
 	cnt++;
-	while (*(s + cnt) != '\0')
+	while (*(str + cnt) != '\0')
 	{
 		for (i = 0; i < 13; i++)
 		{
-			if (*(s + cnt) == word_sep[i])
+			if (*(str + cnt) == word_sep[i])
 			{
-				if ((*(s + (cnt + 1)) > 96) && (*(s + (cnt + 1)) < 123))
-					*(s + (cnt + 1)) -= 32;
+				if ((*(str + (cnt + 1)) > 96) && (*(str + (cnt + 1)) < 123))
+					*(str + (cnt + 1)) -= 32;
 				break;
 			}
 		}
