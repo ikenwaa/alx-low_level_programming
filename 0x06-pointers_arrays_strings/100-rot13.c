88c8bdba6d74bf982704ea83bb56ca382824c2fb
ikenwaa <ajohn9353@gmail.com>
2022-09-22 16:26:26 -0700

    Added 100-rot13.c

---
+++ 0x06-pointers_arrays_strings/100-rot13.c
@@ -0,0 +1,24 @@
+#include "main.h"
+
+/**
+ * rot13 - Encodes a string using rot13 - Rotate 13 places
+ * @str: string to be encoded.
+ * Return: encoded string.
+ */
+
+char *rot13(char *str)
+{
+	char alph[] = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz";
+	char rot[] = "NOPQRSTUVWXYZABCDEFGHIJKLMnopqrstuvwxyzabcdefghijklm";
+	int i = 0, j;
+
+	for (; str[i] != '\0'; i++)
+	{
+		for (j = 0; alph[j] != '\0'; j++)
+		{
+			if (str[i] == alph[j])
+				str[i] = rot[j];
+		}
+	}
+	return (str);
+}
