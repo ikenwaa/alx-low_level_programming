2719a0070d38922e52d36742ec372722d4df7584
ikenwaa <ajohn9353@gmail.com>
2022-09-27 16:47:33 -0700

    Added 7-print_chessboard.c

---
+++ 0x07-pointers_arrays_strings/7-print_chessboard.c
@@ -0,0 +1,18 @@
+#include "main.h"
+
+/**
+ * print_chessboard - Prints a chessboard
+ * @a: number of squares to be printed
+ */
+
+void print_chessboard(char (*a)[8])
+{
+	unsigned int i, j;
+
+	for (i = 0; i <= 7; i++)
+	{
+		for (j = 0; j <= 7; j++)
+			_putchar(a[i][j]);
+	}
+	_putchar('\n');
+}
