d8ea577ab8c678b50a74f82dbbea8839aa96b26d
ikenwaa <ajohn9353@gmail.com>
2022-10-04 17:51:57 -0700

    Added 2-str_concat.c

--- 0x0B-malloc_free/2-str_concat.c
+++ 0x0B-malloc_free/2-str_concat.c
@@ -12,11 +12,10 @@ char *str_concat(char *s1, char *s2)
 	char *str;
 	int i, j, len = 0;
 
-	if (s1 == 0 || s2 == 0)
-	{
+	if (s1 == 0)
 		s1 = "";
+	if (s2 == 0)
 		s2 = "";
-	}
 
 	for (i = 0; s1[i] || s2[i]; i++)
 		len++;
