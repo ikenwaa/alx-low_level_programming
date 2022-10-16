89ab1e91e24889e33fbdef029c186c97fa3c068a
ikenwaa <ajohn9353@gmail.com>
2022-10-16 16:27:16 -0700

    Added 0-print_name.c

--- 0x0F-function_pointers/function_pointers.h
+++ 0x0F-function_pointers/function_pointers.h
@@ -3,7 +3,7 @@
 #include "stdlib.h"
 #include "stdio.h"
 
-void print_name(char  *name, void (*f)(char *));
+void print_name(char *name, void (*f)(char *));
 void array_iterator(int *array, size_t size, void (*action)(int));
 
 #endif
