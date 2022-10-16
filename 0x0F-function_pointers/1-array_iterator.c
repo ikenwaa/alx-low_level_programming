873f1cc57a916f9d3a09fca0a4018f7d35fd3fa2
ikenwaa <ajohn9353@gmail.com>
2022-10-16 16:39:14 -0700

    Added 1-array_iterator.c

---
+++ 0x0F-function_pointers/1-array_iterator.c
@@ -0,0 +1,17 @@
+#include <function_pointers.h>
+
+/**
+ * array_iterator - Iterate through the indexes of an array
+ * @array: Array
+ * @size: Size of the array
+ * @action: pointer to the function
+ */
+
+void array_iterator(int *array, size_t size, void (*action)(int))
+{
+	unsigned int i = 0;
+
+	if (array && action)
+		for (; i < size; i++)
+			action(array[i]);
+}
