4be5b264263d10a2f51a84f5bf98910d06901290
ikenwaa <ajohn9353@gmail.com>
2022-10-20 06:47:43 -0700

    Updated 3-get_op_func.c

--- 0x0F-function_pointers/3-op_functions.c
+++ 0x0F-function_pointers/3-op_functions.c
@@ -30,7 +30,15 @@ int op_mul(int a, int b) { return (a * b); }
  * @b: Divisor
  * Return: Result of a divided by b
  */
-int op_div(int a, int b) { return (a / b); }
+int op_div(int a, int b)
+{
+	if (b == 0)
+	{
+		printf("Error\n");
+		exit(100);
+	}
+	return (a / b);
+}
 
 /**
  * op_mod - Gets the remainder of the division of integers
@@ -38,4 +46,12 @@ int op_div(int a, int b) { return (a / b); }
  * @b: second integer
  * Return: Result of a modulo b
  */
-int op_mod(int a, int b) { return (a % b); }
+int op_mod(int a, int b)
+{
+	if (b == 0)
+	{
+		printf("Error\n");
+		exit(100);
+	}
+	return (a % b);
+}
