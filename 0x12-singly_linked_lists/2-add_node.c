90b1fa135855b9b1ce97df6de44ecfd62f7d0b61
ikenwaa <ajohn9353@gmail.com>
2022-10-23 14:41:25 -0700

    Added 2-add_node.c

---
+++ 0x12-singly_linked_lists/2-add_node.c
@@ -0,0 +1,28 @@
+#include "lists.h"
+
+/**
+ * add_node - Adds a node to head of a list
+ * @head: Double pointer to the list
+ * @str: String to add to the node
+ * Return: Address of new element, or NULL if failed
+ */
+
+list_t *add_node(list_t **head, const char *str)
+{
+	size_t num = 0;
+	list_t new;
+
+	new = malloc(sizeof(list_t));
+	if (!new)
+		return (NULL);
+
+	new->str = strdup(str);
+
+	for (num = 0; str[num]; num++)
+		;
+	new->len = num;
+	new->next = *head;
+	*head = new;
+
+	return (*head);
+}
