3d19e0b45ba5b86a58d7c9e04b8f0485e6e44511
ikenwaa <ajohn9353@gmail.com>
2022-10-31 16:06:03 -0700

    Added 1-create_file.c

--- 0x15-file_io/1-create_file.c
+++ 0x15-file_io/1-create_file.c
@@ -12,7 +12,7 @@ int create_file(const char *filename, char *text_content)
 {
 	int fd, nstr, rwr;
 
-	if (filename == NULL)
+	if (!filename)
 		return (-1);
 
 	fd = open(filename, O_CREAT | O_WRONLY | O_TRUNC, 0600);
@@ -20,7 +20,7 @@ int create_file(const char *filename, char *text_content)
 	if (fd == -1)
 		return (-1);
 
-	if (text_content == NULL)
+	if (!text_content)
 		text_content = "";
 
 	for (nstr = 0; text_content[nstr]; nstr++)
