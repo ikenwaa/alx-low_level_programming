c9212adc05254d7f4dd290626594da22e992a4ea
ikenwaa <ajohn9353@gmail.com>
2022-12-11 03:53:41 -0800

    Edited 0-print_dlistint.c

--- 0x17-doubly_linked_lists/0-print_dlistint.c
+++ 0x17-doubly_linked_lists/0-print_dlistint.c
@@ -1,14 +1,21 @@
 #include "lists.h"
 
+/**
+ * print_dlistint - Prints all the elements in dlistint_t list
+ *
+ * @h: head of the list
+ * Return: all elements in dlistint
+ */
+
 size_t print_dlistint(const dlistint_t *h)
 {
-    size_t el = 0;
+	size_t el = 0;
 
-    while (h)
-    {
-        printf("%d\n", h->n);
-        h = h->next;
-        el ++;
-    }
-    return el;
+	while (h)
+	{
+		printf("%d\n", h->n);
+		h = h->next;
+		el++;
+	}
+	return (el);
 }
